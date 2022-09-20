6e7d9492f77a8dd44fd5398c87bb1a26cc9dc53d
saroj.shrestha <freelancersaroj@gmail.com>
2022-09-20 12:42:49 +0545

    CheckoutController: auth api added

--- app/Http/Controllers/API/MyOrderController.php
+++ app/Http/Controllers/API/MyOrderController.php
@@ -33,7 +33,7 @@ class MyOrderController extends Controller
     public function show(Order $order)
     {   
         try {
-            if( $order->user->id != auth('api')->user()->id )
+            if( $order->user_id != auth('api')->user()->id )
             {
                 throw new \Exception("Invalid Order detail id.");
             }            	            
