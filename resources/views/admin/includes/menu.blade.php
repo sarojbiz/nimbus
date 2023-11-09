89bd5bed019d8983aa72c0013d80f070a034979f
saroj.shrestha <freelancersaroj@gmail.com>
2023-11-09 13:12:16 +0545

    Logo added

--- resources/views/admin/includes/menu.blade.php
+++ resources/views/admin/includes/menu.blade.php
@@ -1,6 +1,6 @@
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('admin.dashboard')}}">
-    <div class="sidebar-brand-text mx-3">Misumi Admin</div>
+    <div class="sidebar-brand-text mx-3">Nimbusbazar Admin</div>
     </a>
     <hr class="sidebar-divider my-0">
     <li class="nav-item active">
