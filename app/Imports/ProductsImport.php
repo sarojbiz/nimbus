ef8366f08df395b0799b52bf6dd84f99a949ceb4
Saroj Shrestha <freelancersaroj@gmail.com>
2024-02-22 12:17:05 +0545

    Product Import updated

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -217,7 +217,7 @@ class ProductsImport implements ToModel, WithStartRow
 		if ( !empty($replace) ) {
 			$str = str_replace((array)$replace, ' ', $str);
 		}
-		$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+		$clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $str);
 		$clean = strtolower(trim($clean, '-'));
 		$clean = preg_replace("/[\/_|+ -]+/", $delimiter, $clean);
 		return $clean;
